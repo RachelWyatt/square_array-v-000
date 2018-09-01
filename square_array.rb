18ef887854ebb7ff1f181b67810a52911dcf1cb5
Rachel Wyatt <rw126@evansville.edu>
2018-09-01 14:03:53 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
- array.each {|number| number**2}
- new_array = []
- new_array << number
+ array.each do |number|
+   number**2
+   new_array = []
+   new_array << number
 end
